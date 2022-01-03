592f6ea3df753c8b58f8bc3bbd0dae9834164247
unknown <sabian329@gmail.com>
2022-01-03 17:54:38 +0100

    add post

--- server.js
+++ server.js
@@ -3,8 +3,13 @@ const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 3000;
+const express = require("express");
+const app = express();
 
 server.use(middlewares);
 server.use(router);
 
 server.listen(port);
+app.post("/", (req, res) => {
+  res.status(200).json({ message: "It worked!" });
+});
